bbf7df382dbe0bc004240c5e69a0cb75c2bcdd82
Jannis Baum <jannis.baum@gmail.com>
2023-12-07 16:13:01 +0100

    refactor: series with individual vectors

--- model/src/graphs.jl
+++ model/src/graphs.jl
@@ -2,21 +2,18 @@ include("gaussians.jl")
 include("factors.jl")
 
 # computes prediction for series subgraph
-# input has rows μ, σ, time passed until next, columns are records
-function evaluate_series(series::Matrix{Float64}, λ::Float64)::Gaussian
-    if size(series)[1] != 3
-        throw(ArgumentError("Input needs to have 3 rows"))
-    end
-    if length(series) == 0
-        throw(ArgumentError("Series is empty"))
-    end
+function evaluate_series(
+    μs::Vector{Float64},  # mean values
+    σ2s::Vector{Float64}, # sigma values
+    ts::Vector{Float64},  # time to next record or end
+    λ::Float64,           # weight
+)::Gaussian
+    result = Gaussian(μs[1], σ2s[1])
+    time_passed!(result, ts[1])
 
-    result = Gaussian(series[1,1], series[2,1])
-    time_passed!(result, series[3, 1])
-
-    for record = eachcol(series)
-        result = weighted_sum(result, 1 - λ, Gaussian(record[1], record[2]), λ)
-        time_passed!(result, record[3])
+    for (μ, σ2, t) = zip(μs, σ2s, ts)
+        result = weighted_sum(result, 1 - λ, Gaussian(μ, σ2), λ)
+        time_passed!(result, t)
     end
 
     return result
