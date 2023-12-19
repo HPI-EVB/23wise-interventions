324c6ac3368e66c9bedbe31f59ed1a5ab5db74f5
Jannis Baum <jannis.baum@gmail.com>
2023-12-19 07:44:31 +0100

    feat: add database helper for eom

---
+++ eom/src/database.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine, text
+import pandas as pd
+import os
+
+db_name = os.getenv('DB_NAME_SEMINAR2')
+db_user = os.getenv('DB_USER')
+db_host = os.getenv('DB_HOST')
+db_pw = os.getenv('DB_PW')
+db_port = os.getenv('DB_PORT')
+
+engine = create_engine(f'postgresql://{db_user}:{db_pw}@{db_host}:{db_port}/{db_name}')
+
+def read_sql(sql: str) -> pd.DataFrame:
+    return pd.read_sql(text(sql), engine)
